f45d74bd07108613b246aa783ba24bdc61f13553
Vishesh-Dwivedi-Git <visheshdwivedi225544@gmail.com>
2024-12-25 23:50:20 +0530

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import gsap from 'gsap'
+import { useGSAP
 
+ } from '@gsap/react'
 const Hero = () => {
+  // const [vedioSrc , setVideoSrc]=useState();
+  useGSAP(()=>{
+    gsap.to('#hero',{
+      opacity:1,
+      delay:1.5
+    })
+  },[]);
+
   return (
     <section className="w-full nav-height bg-black relative">
-      
+      <div className='h-5/6 w-full flex-center flex-col'>
+      <p id="hero" className='hero-title'>
+        iPhone 15 Pro
+      </p>
+      <div className='md:w-10/12 w-9/12'>
+      <vedio>
+        {/* <source src={videoSrc}></source> */}
+      </vedio>
+
+      </div>
+      </div>
     </section>
   )
 }
